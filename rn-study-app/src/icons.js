49e6d0a7b6e62cfc749ee3befce2d6f5d512409f
이승원 <iseung-won@iseung-won-ui-MacBookPro.local>
2022-02-23 06:26:31 +0900

    my first commit

---
+++ rn-study-app/src/icons.js
@@ -0,0 +1,11 @@
+import Uncheck from '../assets/icons/uncheck.png';
+import Check from '../assets/icons/check.png';
+import Delete from '../assets/icons/delete.png';
+import Edit from '../assets/icons/edit.png';
+
+export const icons = {
+  uncheck: Uncheck,
+  check: Check,
+  delete: Delete,
+  edit: Edit,
+};
