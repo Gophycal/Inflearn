82e397b4c1cab6b2e5bae31e5bb6e3ff77c54252
이승원 <iseung-won@iseung-won-ui-MacBookPro.local>
2022-02-28 04:14:47 +0900

    22/02/28 fxxking disgusting function usage change which mk me hard to mk 'log in' function.

--- rn-chat/src/firebase.js
+++ rn-chat/src/firebase.js
@@ -1,4 +1,12 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import config from '../firebase.json';
 
 const app = firebase.initializeApp(config);
+
+const Auth = getAuth(app);
+
+export const signin = async ({ email, password }) => {
+  const { user } = await signInWithEmailAndPassword(Auth, email, password);
+  return user;
+};
